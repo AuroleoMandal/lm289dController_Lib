4bfeb068cf1e7677cc220eb46e56a623578b2c22
Auroleo Mandal <85514952+AuroleoMandal@users.noreply.github.com>
2021-07-15 02:34:34 +0530

    Add files via upload

--- Motor.cpp
+++ Motor.cpp
@@ -1,6 +1,7 @@
 /*
-Library to control motor funtionalities using L298N motor driver
+Library to simplify motor functionalities with lm298d motor driver
 -Auroleo Mandal 12/07/2021
+12/07/21 Currently features basic motor control (3 methods to set motor state to forward, reverse and off)
 */
 #include "Arduino.h"
 #include "Motor.h"
@@ -9,21 +10,42 @@ Motor::Motor(int A1, int A2)
 {
   pinMode(A1, OUTPUT);
   pinMode(A2, OUTPUT);
-  _pin00 = A1;
-  _pin01 = A2;
+  _pin0 = A1;
+  _pin1 = A2;
+}
+Motor::Motor(int A1, int A2, int E)
+{
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(E, OUTPUT);
+  _pin0 = A1;
+  _pin1 = A2;
+  _pinE = E;
 }
 void Motor::fwd()
 {
-  digitalWrite(_pin00, HIGH);
-  digitalWrite(_pin01, LOW);
+  digitalWrite(_pin0, HIGH);
+  digitalWrite(_pin1, LOW);
+}
+void Motor::fwd(int E)
+{
+  digitalWrite(_pin0, HIGH);
+  digitalWrite(_pin1, LOW);
+  analogWrite(_pinE, E);
 }
 void Motor::rev()
 {
-  digitalWrite(_pin00, LOW);
-  digitalWrite(_pin01, HIGH);
+  digitalWrite(_pin0, LOW);
+  digitalWrite(_pin1, HIGH);
+}
+void Motor::rev(int E)
+{
+  digitalWrite(_pin0, LOW);
+  digitalWrite(_pin1, HIGH);
+  analogWrite(_pinE, E);
 }
 void Motor::off()
 {
-  digitalWrite(_pin00, LOW);
-  digitalWrite(_pin01, LOW);
+  digitalWrite(_pin0, LOW);
+  digitalWrite(_pin1, LOW);
 }
--- Motor.h
+++ Motor.h
@@ -1,5 +1,5 @@
 /*
-Library to control motor funtionalities using L298N motor driver
+Library to simplify motor functionalities with lm298d motor driver
 -Auroleo Mandal 12/07/2021
 12/07/21 Currently features basic motor control (3 methods to set motor state to forward, reverse and off)
 */
@@ -10,11 +10,16 @@ class Motor
 {
   public:
   Motor(int A1, int A2);
+  Motor(int A1, int A2, int E);
   void fwd();
+  void fwd(int);
   void rev();
+  void rev(int);
   void off();
+  void off(int);
   private:
-  int _pin00;
-  int _pin01;
+  int _pin0;
+  int _pin1;
+  int _pinE;
 };
 #endif 
